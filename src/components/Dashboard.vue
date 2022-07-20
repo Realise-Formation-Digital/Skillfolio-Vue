8f6a6c3e2a83ec49f2b9c89f925dfcd99003f6d8
Sam <99719826+SamOrion1@users.noreply.github.com>
2022-07-20 15:48:00 +0200

    fix bar

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,37 +1,30 @@
 <template>
-<v-app>
-    <v-toolbar color="primary" dark fixed app>
-        <v-toolbar-title>Application</v-toolbar-title>
-      </v-toolbar>
- <v-navigation-drawer permanent>
-        <v-list>
-          <v-list-item class="px-2">
-            <v-list-item-avatar>
-              <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
-            </v-list-item-avatar>
-          </v-list-item>
+  <v-app>
+    <!-- <v-toolbar color="primary" dark fixed app>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar> -->
+    <v-navigation-drawer permanent>
+      <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
 
-          <v-list-item link>
-            <v-list-item-content>
-              <v-list-item-title class="text-h6">
-                {{username || 'Username'}}
-              </v-list-item-title>
-              <v-list-item-subtitle v-on:click="logout">log out</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="text-h6">
+              {{ username || 'Username' }}
+            </v-list-item-title>
+            <v-list-item-subtitle v-on:click="logout">log out</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
 
       <v-divider></v-divider>
 
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -44,28 +37,65 @@
     </v-navigation-drawer>
 
 
+    <!-- <div class="search-wrapper">
+    <input type="text" v-model="search" placeholder="Search"/>
+        <label>Search title:</label>
+    </div> -->
+
+
     <v-container fluid class="bg-surface-variant">
-      <v-row> 
-          <v-col justify="center" md="4" v-for="item of info" :key="item.id">
-          
-            <li>
-              <b> {{ item.name }} </b>
-              <br />
-              <v-img class="bg-white" height="200" :aspect-ratio="1" :src="item.image_url"></v-img>
-              <br />
-              <div>{{ item.description }}</div>
-              <br />
-                <v-btn block depressed color="error">
+      <v-row>
+        <v-col justify="center" md="4" v-for="item of info" :key="item.id">
+
+          <li>
+            <b> {{ item.name }} </b>
+            <br />
+            <v-img class="bg-white" height="200" :aspect-ratio="1" :src="item.image_url"></v-img>
+            <br />
+            <div>lorem</div>
+            <br />
+            <!-- <v-btn block depressed color="error">
                   --- Plus de details ---
-                </v-btn>
-              <br />
-              <v-divider></v-divider>
-            </li>
-          </v-col>
-        
+                </v-btn> -->
+            <div class="text-center">
+              <v-dialog v-model="dialog" width="500">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
+                    Click Me
+                  </v-btn>
+                </template>
+
+                <v-card>
+                  <v-card-title class="text-h5 grey lighten-2">
+                    Privacy Policy
+                  </v-card-title>
+
+                  <v-card-text>
+                    {{ item.description }}
+                  </v-card-text>
+
+                  <v-divider></v-divider>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" text @click="dialog = false">
+                      Supprimer
+                    </v-btn>
+                    <v-btn color="primary" text @click="dialog = false">
+                      Modifier
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </div>
+            <br />
+            <v-divider></v-divider>
+          </li>
+        </v-col>
+
       </v-row>
     </v-container>
-</v-app>
+  </v-app>
 </template>
 
 <script>
@@ -82,6 +112,7 @@
 
     data () {
       return {
+        dialog: false,
         info : [],
         items: [
           { title: 'Ajouter un compte CF', icon: 'mdi-view-dashboard' },
@@ -96,3 +127,6 @@
     }
   }
 </script>
+
+<style>
+</style>
