34f72f9b839f929511f865485788a03423dae06e
Gab <99719531+GabrielDuarteSousa@users.noreply.github.com>
2022-07-25 11:30:07 +0200

    vuetify config correction

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 Vue.use(Vuetify)
@@ -11,5 +12,6 @@ Vue.use(Vuetify)
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
