9a49ce68765d83e1131d9dc1f4f83ef7bbaa9f04
Sam <99719826+SamOrion1@users.noreply.github.com>
2022-07-25 14:17:33 +0200

    add info profil

--- src/views/MoDalView.vue
+++ src/views/MoDalView.vue
@@ -10,7 +10,20 @@
                 </v-card-title>
 
                 <v-card-text>
-                    <p>Name : {{ item.year }}</p>
+                    <v-list-item-content>
+                    <v-list-item-title>ID : {{ item.id}}</v-list-item-title>
+                    <v-list-item-title>Name : {{ item.name }}</v-list-item-title>
+                    <v-list-item-title>Lastname : {{ item.lastname }}</v-list-item-title>
+                    <v-list-item-title>Age : {{ item.age }}</v-list-item-title>
+                    <v-list-item-title>Year : {{ item.year }}</v-list-item-title>
+                    <v-list-item-title>Departement : {{ item.departament }}</v-list-item-title>
+                    <v-list-item-title>Language : {{ item.language }}</v-list-item-title>
+                    <v-list-item-title>Research : {{ item.Research }}</v-list-item-title>
+                    <v-list-item-title>Type : {{ item.type }}</v-list-item-title>
+                    <v-list-item-title>ID Job Coach : {{ item.idJC }}</v-list-item-title>
+                    <v-list-item-title>ID Conseiller Coach : {{ item.idCC }}</v-list-item-title>
+                    <v-list-item-title>ID Formateur : {{ item.idTeacher }}</v-list-item-title>
+                    </v-list-item-content>
                 </v-card-text>
 
                 <v-divider></v-divider>
